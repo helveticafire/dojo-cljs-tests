30671068ff587df6e400740b32e19c2c3e04dbd4
Ben Fitzgerald <theeuclidean@gmail.com>
2015-07-28 21:04:25 +0100

    Clean up

--- src/dojo/core.cljs
+++ src/dojo/core.cljs
@@ -1,7 +1,6 @@
 (ns dojo.core
-	(:require [cljs.test :refer-macros [deftest testing is]]))
+  (:require [cljs.test :refer-macros [deftest testing is]]))
 
 (.log js/console "hello world")
- 
- (deftest first-test
- 	(is (= 1 1)))
+
+(deftest first-test(is (= 1 1)))
